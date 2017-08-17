50bfa67d6eb2e53f63d9d264d70387c78f7d06f1
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-17 11:58:14 -0700

    Update MultiColumnProcessor.py

--- MultiColumnProcessor.py
+++ MultiColumnProcessor.py
@@ -2,14 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
-"""
-MultiColumnLabelEncoder class can be used to label all categorical colomns in a pandas data-frame
-via preprocessing.LabelEncoder. After this transformation the colomns data-types will remain the same
-as the original ones (e.g. categorical colomns will be still categorical). The object created here 
-(after fitting) can be saved and later be used for other pandas data-frames with the same categorical 
-colomns.
-"""
-
 ##
 class MultiColumnLabelEncoder:
     ##
