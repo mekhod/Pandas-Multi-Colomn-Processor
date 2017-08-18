31a7472ab9202906a372e1516f774bfd7dff8e53
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-18 12:39:11 -0700

    Add files via upload

--- MultiColumnProcessor.py
+++ MultiColumnProcessor.py
@@ -84,7 +84,7 @@ class MultiColumnOneHotEncoder:
     ##
     def __init__(self):
         self.__catColumns = []
-        self.OneHotEncoder = {}
+        self.__MultiOHE = {}
 
     ##
     def __getCategoryColomns(self, data=pd.DataFrame()):
@@ -107,20 +107,46 @@ class MultiColumnOneHotEncoder:
         for col in self.__catColumns:
             OneHotEncoder = preprocessing.OneHotEncoder(sparse=False)
             OneHotEncoder.fit(np.array(data.loc[:, col]).reshape(-1, 1))
-            self.OneHotEncoder[col] = OneHotEncoder
+            self.__MultiOHE[col] = OneHotEncoder
         ##
         return self
 
     def transform(self, data):
+
         ##
-        transformedCols = data.drop(self.__catColumns, axis=1)
+        catData = data[self.__catColumns]
+        data = data.drop(self.__catColumns, axis=1)
+
         ##
-        for col in self.__catColumns:
-            OneHotEncoder = self.OneHotEncoder[col]
-            transformed = OneHotEncoder.transform(np.array(data.loc[:, col]).reshape(-1, 1))
-            transformed = pd.DataFrame(transformed)
-            transformed.columns = [str(col) + '_' + str(c) for c in transformed.columns]
-            transformed.set_index(transformedCols.index, inplace=True)
-            transformedCols = pd.concat([transformedCols, transformed], axis=1)
+        def Transform_Rec(dta=catData):
+            ##
+            nCol = dta.shape[1]
+            ##
+            if nCol == 1:
+                ##
+                col = dta.columns[0]
+                OneHotEncoder = self.__MultiOHE[col]
+                transformed = OneHotEncoder.transform(np.array(dta.loc[:, col]).reshape(-1, 1))
+                transformed = pd.DataFrame(transformed)
+                transformed.columns = [str(col) + '_' + str(c) for c in transformed.columns]
+                ##
+                return transformed
+
+            else:
+                ##
+                if (nCol % 2 == 0):
+                    middle_index = int(nCol / 2)
+                else:
+                    middle_index = int(nCol / 2 - 0.5)
+                ##
+                left = dta.iloc[:, :middle_index]
+                right = dta.iloc[:, middle_index:]
+                ##
+                return pd.concat([Transform_Rec(dta=left), Transform_Rec(dta=right)], axis=1)
+
         ##
-        return transformedCols
+        transformedCatData = Transform_Rec(dta=catData)
+        transformedCatData.set_index(data.index, inplace=True)
+
+        ##
+        return pd.concat([data, transformedCatData], axis=1)
