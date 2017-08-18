04317ebb93057adc89d37bc81c043e1c51819082
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-18 11:35:18 -0700

    Add files via upload

--- MultiColumnProcessor.py
+++ MultiColumnProcessor.py
@@ -35,23 +35,49 @@ class MultiColumnLabelEncoder:
     ##
     def transform(self, data):
         ##
-        transformedCols = data.drop(self.__catColumns, axis=1)
+        catData = data[self.__catColumns]
+        data = data.drop(self.__catColumns, axis=1)
+
         ##
-        for col in self.__catColumns:
-            le = self.__MultiLE[col]
-            transformed = le.transform(data.loc[:, col])
-            transformed = pd.DataFrame(transformed)
-            transformed.columns = [col]
-            transformed.set_index(transformedCols.index, inplace=True)
-            transformedCols = pd.concat([transformedCols, transformed], axis=1)
+        def Transform_Rec(dta=catData):
+            ##
+            nCol = dta.shape[1]
+            ##
+            if nCol == 1:
+                col = dta.columns[0]
+                le = self.__MultiLE[col]
+                transformed = le.transform(dta.iloc[:, 0])
+                transformed = pd.DataFrame({col: transformed})
+                ##
+                return transformed
+
+            else:
+                ##
+                if (nCol % 2 == 0):
+                    middle_index = int(nCol / 2)
+                else:
+                    middle_index = int(nCol / 2 - 0.5)
+                ##
+                left = dta.iloc[:, :middle_index]
+                right = dta.iloc[:, middle_index:]
+                ##
+                return pd.concat([Transform_Rec(dta=left), Transform_Rec(dta=right)], axis=1)
+
+        ##
+        catData = Transform_Rec(dta=catData)
+        catData.set_index(data.index, inplace=True)
+
+        ##
+        data = pd.concat([data, catData], axis=1)
+
         ##
         for i, j in self.dataTypes.items():
             try:
-                transformedCols[i] = transformedCols[i].astype(j)
+                data[i] = data[i].astype(j)
             except:
                 pass
         ##
-        return transformedCols
+        return data
 
 ##
 class MultiColumnOneHotEncoder:
