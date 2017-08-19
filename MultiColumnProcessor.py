c2d238521c4269388663f93c8ba68b5394fba433
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-19 10:37:44 -0700

    Add files via upload

--- MultiColumnProcessor.py
+++ MultiColumnProcessor.py
@@ -150,3 +150,28 @@ class MultiColumnOneHotEncoder:
 
         ##
         return pd.concat([data, transformedCatData], axis=1)
+
+##
+class MultiColumnScaler:
+        ##
+        def __init__(self):
+            self.scaler = object()
+
+        ##
+        def fit(self, data):
+            ##
+            self.scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+            self.scaler.fit(data)
+            ##
+            return self
+
+        ##
+        def transform(self, data):
+            ##
+            columns = data.columns.tolist()
+            ##
+            data = pd.DataFrame(self.scaler.transform(data.as_matrix()))
+            ##
+            data.columns = columns
+            ##
+            return data
