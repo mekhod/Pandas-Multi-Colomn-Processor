049405d34b454373dd1631042383c658a2b04ff9
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-19 12:20:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ setup(
   name = 'MultiColumnProcessor',
   packages = ['MultiColumnProcessor'],
   version = '0.1',
-  description = 'A simple package to process pandas colomns using',
-  author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
+  description = 'a simple package to process pandas categorical colomns via preprocessing in scikit-learn',
+  author = 'Mehdi Khodayari',
+  author_email = 'me.khodayari@gmail.com',
   url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
   download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
