0d7984d7b18b1a3b11c81454cb27f65154e20833
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-19 12:22:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
   description = 'a simple package to process pandas categorical colomns via preprocessing in scikit-learn',
   author = 'Mehdi Khodayari',
   author_email = 'me.khodayari@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/mekhod/Pandas-Multi-Colomn-Processor.git',
+  #download_url = 'https://github.com/mekhod/Pandas-Multi-Colomn-Processor/archive/0.1.tar.gz',
+  keywords = ['label encoder', 'multi colomns'],
   classifiers = [],
 )
