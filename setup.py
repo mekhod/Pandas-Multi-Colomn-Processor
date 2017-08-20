25134d2f4bb7adb7da1bc54f6ff20602fa5e48f7
Mehdi Khodayari <me.khodayari@gmail.com>
2017-08-20 14:20:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'MultiColProcessor',
   packages = ['MultiColProcessor'],
-  version = '1.0.13',
+  version = '1.0.14',
   description='a simple package to process pandas categorical colomns via preprocessing in scikit-learn',
   url='https://github.com/mekhod/Pandas-Multi-Colomn-Processor',
   author='Mehdi Khodayari',
